cb99010c4336bf5f4ff94c2517bdcfdecdb097c3
Sandhyaa-kumar <ksandhyaa27@gmail.com>
2025-07-18 15:47:20 +0530

    Removed submodule and added as normal folder

---
+++ leetcode/TwoPointers/ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+package leetcode.TwoPointers;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int i=0,j=height.length-1,max=Integer.MIN_VALUE,area=0;
+        while(i<j){
+             area=Math.max(area,Math.min(height[i],height[j])*(j-i));
+             if(height[i]<height[j]){
+                i++;
+             }
+             else
+             j--;
+        }
+        return area;
+    }
+    public static void main (String[] args)
+    {
+       
+      int[] arr = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+      ContainerWithMostWater obj = new ContainerWithMostWater();
+      int ans = obj.maxArea(arr);
+      System.out.println("Max Area = " + ans);
+    }
+}
