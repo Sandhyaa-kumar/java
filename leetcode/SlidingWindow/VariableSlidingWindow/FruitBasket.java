7a7676d979cf2ffef09b760923d24a6fb0b91527
Sandhyaa-kumar <ksandhyaa27@gmail.com>
2025-07-30 15:41:10 +0530

    added

---
+++ leetcode/SlidingWindow/VariableSlidingWindow/FruitBasket.java
@@ -0,0 +1,27 @@
+package leetcode.SlidingWindow.VariableSlidingWindow;
+
+import java.util.HashMap;
+
+public class FruitBasket {
+    
+    public int totalFruit(int[] fruits) {
+        int left=0,max=Integer.MIN_VALUE;
+        HashMap <Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<fruits.length;i++)
+        {
+            map.put(fruits[i],map.getOrDefault(fruits[i],0)+1);
+            while(map.size()>2)
+            {
+               map.put(fruits[left],map.get(fruits[left])-1);
+               if(map.get(fruits[left])==0)
+               {
+                map.remove(fruits[left]);
+               }
+                left++;
+            }
+            max=Math.max(max,i-left+1);
+        }
+        return max;
+    }
+}
+
